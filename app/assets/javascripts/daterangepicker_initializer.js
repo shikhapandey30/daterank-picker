725028f3f15c8d595c286f919ec6a3b282af10ab
arvind kushwah <arvindkushwah9@gmail.com>
2019-06-04 11:21:37 +0530

    first commit

--- app/assets/javascripts/daterangepicker_initializer.js
+++ app/assets/javascripts/daterangepicker_initializer.js
@@ -1,16 +1,30 @@
+// /* globals daterangepicker, $ */
+// "use strict";
+
+// $(function(){
+//   $('[data-behavior=daterangepicker]').daterangepicker({
+//     locale: { format: 'DD/MM/YYYY' },
+//     singleDatePicker: 'true',
+//     cancelLabel: 'Clear',
+//     "timePicker": true,
+//     "singleDatePicker": true,
+//     "timePickerIncrement": 1,
+//     "autoApply": true,
+//     "format": 'YYYY-MM-DD HH:mm:ss'
+//   });
+
+//   $('[data-behavior=daterangepicker]').on('cancel.daterangepicker', function(){
+//     $(this).val(' ');
+//   });
+
+// });
 /* globals daterangepicker, $ */
 "use strict";
 
 $(function(){
   $('[data-behavior=daterangepicker]').daterangepicker({
-    locale: { format: 'DD/MM/YYYY' },
-    singleDatePicker: 'true',
-    cancelLabel: 'Clear',
-    "timePicker": true,
-    "singleDatePicker": true,
-    "timePickerIncrement": 1,
-    "autoApply": true,
-    "format": 'YYYY-MM-DD HH:mm:ss'
+    locale: { format: 'DD/MM/YYYY'},
+    cancelLabel: 'Clear'
   });
 
   $('[data-behavior=daterangepicker]').on('cancel.daterangepicker', function(){
